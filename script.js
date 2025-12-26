f06923e4392cc98f203bebc048c616c144203e59
cozumyapienerji-bit <cozumyapienerji@gmail.com>
2025-12-26 12:15:23 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,104 @@
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+const box = 20;
+let score = 0;
+
+const headImg = new Image();
+headImg.src = "snake_head.png";
+
+const bodyImg = new Image();
+bodyImg.src = "snake_body.png";
+
+const appleImg = new Image();
+appleImg.src = "apple.png";
+
+let snake;
+let direction;
+let food;
+let game;
+
+function init() {
+  snake = [{ x: 9 * box, y: 9 * box }];
+  direction = "RIGHT";
+  score = 0;
+  document.getElementById("score").innerText = "Skor: 0";
+  food = randomFood();
+}
+
+function randomFood() {
+  return {
+    x: Math.floor(Math.random() * 17 + 1) * box,
+    y: Math.floor(Math.random() * 17 + 1) * box
+  };
+}
+
+document.addEventListener("keydown", e => {
+  if (e.key === "ArrowLeft" && direction !== "RIGHT") direction = "LEFT";
+  if (e.key === "ArrowUp" && direction !== "DOWN") direction = "UP";
+  if (e.key === "ArrowRight" && direction !== "LEFT") direction = "RIGHT";
+  if (e.key === "ArrowDown" && direction !== "UP") direction = "DOWN";
+});
+
+function setDirection(dir) {
+  if (dir === "left" && direction !== "RIGHT") direction = "LEFT";
+  if (dir === "up" && direction !== "DOWN") direction = "UP";
+  if (dir === "right" && direction !== "LEFT") direction = "RIGHT";
+  if (dir === "down" && direction !== "UP") direction = "DOWN";
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  for (let i = 0; i < snake.length; i++) {
+    if (i === 0) {
+      ctx.drawImage(headImg, snake[i].x, snake[i].y, box, box);
+    } else {
+      ctx.drawImage(bodyImg, snake[i].x, snake[i].y, box, box);
+    }
+  }
+
+  ctx.drawImage(appleImg, food.x, food.y, box, box);
+
+  let headX = snake[0].x;
+  let headY = snake[0].y;
+
+  if (direction === "LEFT") headX -= box;
+  if (direction === "UP") headY -= box;
+  if (direction === "RIGHT") headX += box;
+  if (direction === "DOWN") headY += box;
+
+  if (headX === food.x && headY === food.y) {
+    score++;
+    document.getElementById("score").innerText = "Skor: " + score;
+    food = randomFood();
+  } else {
+    snake.pop();
+  }
+
+  const newHead = { x: headX, y: headY };
+
+  if (
+    headX < 0 || headY < 0 ||
+    headX >= canvas.width || headY >= canvas.height ||
+    collision(newHead, snake)
+  ) {
+    clearInterval(game);
+    alert("Oyun Bitti!");
+  }
+
+  snake.unshift(newHead);
+}
+
+function collision(head, array) {
+  return array.some(part => part.x === head.x && part.y === head.y);
+}
+
+function restartGame() {
+  clearInterval(game);
+  init();
+  game = setInterval(draw, 120);
+}
+
+init();
+game = setInterval(draw, 120);
